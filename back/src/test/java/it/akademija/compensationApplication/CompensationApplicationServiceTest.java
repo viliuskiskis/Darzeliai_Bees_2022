f3304319c84f384e3eef7df62316e5ac46ff6f1d
Lukas <lukaspetraitis90@gmail.com>
2022-03-19 13:06:51 +0200

    initial commit

--- back/src/test/java/it/akademija/compensationApplication/CompensationApplicationServiceTest.java
+++ back/src/test/java/it/akademija/compensationApplication/CompensationApplicationServiceTest.java
@@ -1,73 +1,58 @@
-//package it.akademija.compensationApplication;
-//
-//import static org.junit.Assert.assertTrue;
-//
-//import java.time.LocalDate;
-//
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.junit.jupiter.api.Order;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import it.akademija.compensationApplication.kindergartenData.KindergartenDataDTO;
-//import it.akademija.user.UserDTO;
-//import it.akademija.user.UserService;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class CompensationApplicationServiceTest {
-//	
-//	@InjectMocks
-//	private CompensationApplicationService compensationApplicationService;
-//
-//	@Mock
-//	private UserService userService;
-//
-//	@Mock
-//	private CompensationApplicationDAO compensationApplicationDAO;
-//	
-//	@Test
-//	void testCreateNewCompensationApplication() {
-//		
-//		UserDTO newUser = new UserDTO(
-//				"USER", 
-//				"Test", 
-//				"Tester", 
-//				"22345678989", 
-//				"Address 1", 
-//				"+37061398876",
-//				"test@user.lt", 
-//				"test@user.lt", 
-//				"test@user.lt");
-//		userService.createUser(newUser);
-//		
-//		KindergartenDataDTO kindergartenDataDTO = new KindergartenDataDTO(
-//				"Test kindergarten", 
-//				"123456789", 
-//				"+37061398876", 
-//				"testKindergarten@email.com", 
-//				"Test Address", 
-//				"AB123456789123456789", 
-//				"AZSAZS235", 
-//				"TestBank");
-//		
-//		CompensationApplicationDTO compensationApplicationDTO = new CompensationApplicationDTO(
-//				LocalDate.of(2019, 5, 5), 
-//				"Tomas", 
-//				"51913245685", 
-//				"Tomulis", 
-//				kindergartenDataDTO, 
-//				newUser);
-//		
-//		compensationApplicationService
-//				.createNewCompensationApplication(compensationApplicationDTO);
-//	
-//		assertTrue(compensationApplicationService
-//					.childExistsByPersonalCode("51913245685"));
-//	}
+package it.akademija.compensationApplication;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import it.akademija.compensationApplication.kindergartenData.KindergartenDataDTO;
+import it.akademija.user.UserDTO;
+
+@ExtendWith(MockitoExtension.class)
+public class CompensationApplicationServiceTest {
+	
+	@Autowired
+	private CompensationApplicationService compensationApplicationService;
+		
+	UserDTO userDTO = new UserDTO(
+		"USER", 
+		"Test", 
+		"Tester", 
+		"22345678989", 
+		"Address 1", 
+		"+37061398876",
+		"test@user.lt", 
+		"test@user.lt", 
+		"test@user.lt");
+	
+	KindergartenDataDTO kindergartenDataDTO = new KindergartenDataDTO(
+		"Test kindergarten", 
+		"123456789", 
+		"+37061398876", 
+		"testKindergarten@email.com", 
+		"Test Address", 
+		"AB123456789123456789", 
+		"AZSAZS235", 
+		"TestBank");
+	
+	CompensationApplicationDTO compensationApplicationDTO = 
+				new CompensationApplicationDTO(
+				LocalDate.of(2019, 5, 5), 
+				"Tomas", 
+				"51913245685", 
+				"Tomulis", 
+				kindergartenDataDTO, 
+				userDTO);
+	
+	@Test
+	public void testCreateNewCompensationApplication() {
+		
+	
+	}
+
 	
 //	@Test
 //	@Order(2)
@@ -131,4 +116,4 @@
 //		
 //		verify(compensationApplicationDAO, times(1)).deleteById(eq(id));
 //	}
-//}
+}
